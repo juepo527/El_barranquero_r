26a331ef6f6fdc0435b7c5d3d965c1b258a96654
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2023-06-01 16:35:15 -0500

    cositas del header

--- PIA/script/script.js
+++ PIA/script/script.js
@@ -1,18 +1,4 @@
-const form = document.getElementById('login-form');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita que el formulario se envíe automáticamente
-
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  if (email === 'usuario@example.com' && password === 'contraseña') {
-    //  redirigir a la página de inicio privada
-    window.location.href = 'privado.html';
-  } else {
-    //  mostrar mensaje de error
-    const error = document.createElement('div');
-    error.innerHTML = 'Correo electrónico o contraseña incorrectos';
-    form.appendChild(error);
-  }
-});
+window.addEventListener('scroll', e => {
+    var nav = document.querySelector('.navbar')
+    nav.classList.toggle('bajar',window.scrollY>0)
+})
