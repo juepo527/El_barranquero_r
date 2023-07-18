d5e532ca1b5d3aae22d87779746e922aa675ae9b
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2023-07-17 22:42:26 -0500

    Instalé y vinculé boostrap al archivo App

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MyComponent from './index.jsx';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    <App></App>
 );
