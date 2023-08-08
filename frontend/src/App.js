44d7e10c1113ad246b6cd77d421ff8438fe9a311
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2023-08-08 14:24:47 -0500

    comp-inicio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import Header from './componentes/header'
+import './App.css';
+import Header from './componentes/header';
+import Body_inicio from './componentes/body_inicio';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Body_inicio></Body_inicio>
     </div>
   );
 }
---
+++ frontend/src/componentes/body_inicio.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import "./body_inicio.css"
+
+function Body_inicio() {
+    return (
+        <div>
+            <section class="hero">
+                <h1>Bienvenidos a nuestro restaurante </h1>
+                <p>
+                    Disfruta de nuestro delicioso menú
+                </p>
+                <a href="menu.html" class="btn-cta">Ver menú</a>
+            </section>
+
+            <section class="about">
+                <h2>Sobre nosotros</h2>
+                <p>
+                    Somos un restaurante campestre ubicado en un entorno natural y
+                    tranquilo.
+                </p>
+                <a href="#" class="btn-cta">Conoce más</a>
+            </section>
+
+            <section class="menu">
+                <h2>Menú del día</h2>
+                <div class="menu-items">
+                    <div class="menu-item">
+                        <img src="#" alt="Plato 1" />
+                        <h3>Nombre del plato 1</h3>
+                        <p>Descripción del plato 1</p>
+                        <p>Precio:</p>
+                    </div>
+                    <div class="menu-item">
+                        <img src="#" alt="Plato 2" />
+                        <h3>Nombre del plato 2</h3>
+                        <p>Descripción del plato 2</p>
+                        <p>Precio:</p>
+                    </div>
+                    <div class="menu-item">
+                        <img src="#" alt="Plato 3" />
+                        <h3>Nombre del plato 3</h3>
+                        <p>Descripción del plato 3</p>
+                        <p>Precio:</p>
+                    </div>
+                </div>
+                <h2>Califica nuestro restaurante</h2>
+                <p>¿Cómo te pareció nuestra comida y servicio? ¡Danos tu opinión!</p>
+                <div class="rating">
+                    <input type="radio" name="rating" value="1" id="1"/><label for="1">★</label>
+                    <input type="radio" name="rating" value="2" id="2"/><label for="2">★</label>
+                    <input type="radio" name="rating" value="3" id="3"/><label for="3">★</label>
+                    <input type="radio" name="rating" value="4" id="4"/><label for="4">★</label>
+                    <input type="radio" name="rating" value="5" id="5"/><label for="5">★</label>
+                </div>
+                <p>¡Gracias por tu opinión!</p>
+                <a href="menu.html" class="btn-cta">Ver menú completo</a>
+            </section>
+        </div>
+    )
+}
+
+export default Body_inicio
