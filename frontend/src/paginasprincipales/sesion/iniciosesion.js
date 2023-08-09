59f35b9f6fefb03231368f3081793cb0518ff724
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2023-08-09 16:45:04 -0500

    Inicio de sesion chambon

--- frontend/src/paginasprincipales/sesion/iniciosesion.js
+++ frontend/src/paginasprincipales/sesion/iniciosesion.js
@@ -5,8 +5,18 @@ import { Link, Outlet } from 'react-router-dom';
 function Iniciosesion() {
   return (
     <div>
-      <Link to={'/admin'}>Admin</Link>
-      <Outlet />
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Inicio de sesión</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" />
+        </div>
+        <Link to={'/admin'}><button type="submit" class="btn btn-primary">Sign in</button></Link>
+        <Outlet />
+      </form>
     </div>
   )
 }
