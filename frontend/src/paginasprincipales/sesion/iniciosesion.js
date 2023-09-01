2af1aa322b21dcbed28b22ceeb9b6a79d5c97dda
imthekko <105671657+santZzz1@users.noreply.github.com>
2023-09-01 15:57:15 -0500

    inicio

--- frontend/src/paginasprincipales/sesion/iniciosesion.js
+++ frontend/src/paginasprincipales/sesion/iniciosesion.js
@@ -1,24 +1,37 @@
-import React from 'react'
-import './iniciosesion.css'
-import { Link, Outlet } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import "./iniciosesion.css";
 
-function Iniciosesion() {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <form>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">Inicio de sesión</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        <Link to={'/admin'}><button type="submit" className="btn btn-primary">Sign in</button></Link>
-        <Outlet />
-      </form>
-    </div>
-  )
-}
+    <body className="binicio">
+      <div className="inicio-sesion" id="inicio-sesion">
+        <form className="forminicio" onSubmit={handleSubmit}>
+          <h1>Iniciar sesión</h1>
+          <input
+            type="email"
+            placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Link to={'/admin'}><button type="submit" className="btn btn-primary">Sign in</button></Link>
+          <Outlet />
+        </form>
+      </div> 
+    </body>
+  );
+};
 
-export default Iniciosesion
+export default Login;
