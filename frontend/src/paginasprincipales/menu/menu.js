c597a3f7230af082c35c06bc12d92ba8427c51eb
SamuelMarti22 <samuelmartinezarteaga22@gmail.com>
2023-09-25 23:23:11 -0500

    medio carrito (agrega y elimina productos y css)

--- frontend/src/paginasprincipales/menu/menu.js
+++ frontend/src/paginasprincipales/menu/menu.js
@@ -8,21 +8,34 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function Menu() {
 
-  const onAddProduct =()=>{
-    console.log('add')
-  }
-  const [precioT, setPrecioT] = useState(0)
+  let [precioT, setPrecioT] = useState(0)
   const [productos, setProductos] = useState([])
   const [menu, setMenu] = useState([])
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const onAddProduct = (val) => {
+    productos.push(val)
+    console.log(productos)
+  }
+
   const getMenu = () => {
     Axios.get('http://localhost:3001/menu',).then((response) => {
       setMenu(response.data);
     });
   }
+
+  const calculateTotalPrice = () => {
+      const sum = productos.reduce((accumulator, product) => accumulator + product.precio, 0);
+      setPrecioT(sum);
+  }
+
+  const eliminarProducto = (val) =>{
+    let index=productos.indexOf(val);
+    productos.splice(index,1)
+  }
+
   getMenu();
 
   return (
@@ -35,14 +48,21 @@ function Menu() {
 
       <Offcanvas show={show} onHide={handleClose} placement={'end'}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          <Offcanvas.Title>Lista de Productos</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          {()=>{if(productos.length == 0){
-            return <div key={productos[0]}><h2>No hay productos en el carrito.</h2></div>}
-            else{
-              return  <h2>{productos}</h2>
-          }}}
+          <div>
+            <ul className='listaCarrito'>
+              {productos.map((producto) => (
+                <li key={producto.id_plato}>
+                  {producto.nombre} ${producto.precio}
+                  <button className="btn btn-danger" onClick={()=>{eliminarProducto(producto)}}>Eliminar producto</button>
+                </li>
+              ))}
+            </ul>
+            <button onClick={calculateTotalPrice}>Calcular Total</button>
+            <p>Total: ${precioT}</p>
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
       <div className='Cards'>
@@ -54,10 +74,10 @@ function Menu() {
                   {val.nombre}
                 </h3>
                 <p className="card-hover__text" alt={val.nombre}>{val.descripcion}</p>
-                <a href="#" className="card-hover__link">
+                <a className="card-hover__link">
                   <span>{val.precio}$</span>
-                  <button className='boton_producto' onClick={()=>{
-                    onAddProduct()
+                  <button className='boton_producto' onClick={() => {
+                    onAddProduct(val)
                   }}><img className='carrito_icon' src='https://res.cloudinary.com/dwjwb4mza/image/upload/v1695064965/cart-shopping-solid_xqp7j5.svg'></img></button>
                 </a>
               </div>
